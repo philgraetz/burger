f28db78e85ff5f9bacdde8f7246e0eb529e17a19
philg <phil.graetz@outlook.com>
2018-07-26 18:27:04 -0500

    All working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,13 +25,6 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // =============================================================
 
-// My 'express.static' above doesn't seem to work...
-// app.get("/public/assets/:file", function(req, res) {
-//     let file = path.join(__dirname, '../public/assets/') + req.params.file;
-//     console.log("Sending file: " + file);
-//     res.sendFile(file);
-// });
-
 // Home - same as '/index'
 app.get("/", function(req, res) {
     res.redirect("/index");
@@ -47,8 +40,6 @@ app.get("/index", function(req, res) {
 // Add a new burger with 'name'
 app.post("/api/burger", function(req, res) {
     let name = req.body.name;
-    console.log(req.body);
-    console.log("name: "+ name);
     burger.addBurger(name, function() {
         // I want it to redirect to '/index',but this doesn't work. 
         // Instead the client JS sets window.location = '/index'
@@ -60,7 +51,6 @@ app.post("/api/burger", function(req, res) {
 // Update burger with 'id' to devoured = true
 app.put("/api/burger", function(req, res) {
     let id = req.body.id;
-    console.log("PUT id: " + id);
     burger.devourBurger(id, function() {
         // I want it to redirect to '/index',but this doesn't work. 
         // Instead the client JS sets window.location = '/index'
