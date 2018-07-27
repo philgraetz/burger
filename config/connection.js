defde0e2163e7e5613b24e361794651e06b94ea6
philg <phil.graetz@outlook.com>
2018-07-26 19:25:42 -0500

    connection.js changes for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 // mysql connection to burgers_db
 let mysql = require('mysql');
-let connection = mysql.createConnection( {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database:  'burgers_db'
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection( {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database:  'burgers_db'
+    });
+}
 connection.connect(function(err) {
     if (err)
         return console.log("ERROR " + err.stack);
